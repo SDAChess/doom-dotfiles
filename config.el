a7460dc1720bb422008bcd7c64c26746d23c74e4
Simon Scatton <simon.scatton@epita.fr>
2021-03-24 19:17:24 +0100

    Last commit before moving to ssd

--- config.el
+++ config.el
@@ -6,9 +6,6 @@
 
 (add-hook 'MAJOR-MODE-local-vars-hook #'lsp!)
 
-(after! lsp-ui
-  (setq lsp-ui-doc-enable t))
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Simon Scatton"
@@ -62,11 +59,12 @@
 ;;
 ;; + `doom-font'
 ;; + `doom-variable-pitch-font'
-(setq doom-font (font-spec :family "Fira Code" :size 20))
+(setq doom-font (font-spec :family "Monoid" :size 20))
 ;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
 ;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+(setq display-line-numbers-type 'relative)
+; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
@@ -74,7 +72,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-city-lights)
+(setq doom-theme 'doom-horizon)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
